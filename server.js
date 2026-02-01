51fef0808be725a1c25945344ac6f952dbe972f4
KhasanMeliev <khasanmeliev2@gmail.com>
2026-02-01 20:26:53 +0500

    added changes

--- server.js
+++ server.js
@@ -1,38 +1,50 @@
+require("dotenv").config({ quiet: true, path: "./.env" });
 const express = require("express");
 const app = express();
 const jwt = require("jsonwebtoken");
-const dotenv = require("dotenv");
-dotenv.config({ quiet: true, path: "./.env" });
-
-const posts = [
-  {
-    username: "hasan",
-    title: "post 1",
-  },
-  {
-    username: "alia",
-    title: "post 2",
-  },
-];
+const cookieParser = require("cookie-parser");
 
 app.use(express.json());
+app.use(cookieParser());
 
-const authenticateToken = (req, res, next) => {
-  const authHeader = req.headers["authorization"];
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  if (username == "admin" && password == "123") {
+    const token = jwt.sign({ username }, process.env.SECRET_KEY, {
+      expiresIn: "1h",
+    });
+    res.cookie("token", token);
 
-  const token = authHeader && authHeader.split(" ")[1];
+    return res.json({ token });
+  }
 
-  if (token == null) return res.sendStatus(401);
-
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403);
-    req.user = user;
-    next();
-  });
-};
-
-app.get("/posts", authenticateToken, (req, res) => {
-  res.json(posts.filter((post) => post.username === req.user.name));
+  res.status(401).send("Wrong credentials");
 });
 
-app.listen(3000);
+function auth(req, res, next) {
+  // const token = req.headers.authorization?.split(" ")[1];
+  const token = req.cookies.token;
+
+  if (!token) return res.status(401).send("No Token");
+
+  try {
+    const decoded = jwt.verify(token, process.env.SECRET_KEY);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(401).send("Invalid Token");
+  }
+}
+
+app.get("/home", auth, (req, res) => {
+  res.send(`Welcome ${req.user.username}`);
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("token");
+  res.send("Logged out");
+});
+
+app.listen(3000, () => {
+  console.log("Server is working on PORT: 3000");
+});
