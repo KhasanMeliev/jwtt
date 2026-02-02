2575f2e2d9381496b67e6b2c135ff71c1191d1a2
KhasanMeliev <khasanmeliev2@gmail.com>
2026-02-02 20:48:08 +0500

    added session

--- server.js
+++ server.js
@@ -1,49 +1,76 @@
-require("dotenv").config({ quiet: true, path: "./.env" });
+require("dotenv").config({ quiet: true });
 const express = require("express");
 const app = express();
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
+const session = require("express-session");
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: "sample-secret",
+    resave: false,
+    saveUninitialized: false,
+  }),
+);
 
-app.post("/login", (req, res) => {
-  const { username, password } = req.body;
-  if (username == "admin" && password == "123") {
-    const token = jwt.sign({ username }, process.env.SECRET_KEY, {
-      expiresIn: "1h",
-    });
-    res.cookie("token", token);
-
-    return res.json({ token });
+app.get("/visit", (req, res) => {
+  if (req.session.page_views) {
+    req.session.page_views++;
+    res.send(`You visited this page ${req.session.page_views} times`);
+  } else {
+    req.session.page_views = 1;
+    res.send("You visited this page first time");
   }
-
-  res.status(401).send("Wrong credentials");
 });
+
+app.get("/logout", (req, res) => {
+  res.session.destroy();
+  res.send("Successfully Logged Out");
+});
+
+// app.post("/login", (req, res) => {
+//   const { username, password } = req.body;
+//   if (username == "admin" && password == "123") {
+//     const token = jwt.sign({ username }, process.env.SECRET_KEY, {
+//       expiresIn: "1h",
+//     });
+
+//     res.cookie("token", token, {
+//       httpOnly: true,
+//       secure: false,
+//       sameSite: "lax",
+//     });
+
+//     return res.send("Successfully logged in");
+//   }
+
+//   res.send("Wrong credentials!");
+// });
 
 // function auth(req, res, next) {
 //   const token = req.cookies.token;
-
-//   if (!token) return res.status(401).send("No Token");
+//   if (!token) {
+//     return res.status(401).send("No token");
+//   }
 
 //   try {
 //     const decoded = jwt.verify(token, process.env.SECRET_KEY);
 //     req.user = decoded;
 //     next();
 //   } catch (error) {
-//     res.status(401).send("Invalid Token");
+//     return res.json({ error: error });
 //   }
+
+//   next();
 // }
 
-app.get("/home", auth, (req, res) => {
-  res.send(`Welcome ${req.user.username}`);
-});
-
-app.post("/logout", (req, res) => {
-  res.clearCookie("token");
-  res.send("Logged out");
-});
+// app.get("/home", auth, (req, res) => {
+//   res.send(`Welcome ${req.user.username} to the home page`);
+// });
 
 app.listen(3000, () => {
-  console.log("Server is working on PORT: 3000");
+  console.log("Server is running on PORT: 3000");
 });
